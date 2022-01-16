1432ee3eefbd52a203c3fc394d8e3708b14f0da9
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 17:23:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Given n as input, print the following pattern.
+Input: n=4
+Output:
+y1 
+y1y2 
+y1y2y3 
+y1y2y3y4
+"""
+
+n = 4
+pt = "y"
+st = ""
+for i in range (1, n+1):
+  st = st + pt + str(i)
+  print(st)
+
